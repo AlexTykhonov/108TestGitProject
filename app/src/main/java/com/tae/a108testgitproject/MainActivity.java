2e84af68696f2eab6570d09649fca8f656379d7f
AlexTykhonov <alexey.tykhonov@gmail.com>
2019-02-24 11:44:19 +0200

    first commit

---
+++ app/src/main/java/com/tae/a108testgitproject/MainActivity.java
@@ -0,0 +1,14 @@
+package com.tae.a108testgitproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+//first change after 1st commit for f
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
